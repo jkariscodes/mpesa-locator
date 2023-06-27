68b43203b0cbf930aed854f39a77cfe73634322f
Joseph Kariuki <joehene@gmail.com>
2023-06-27 18:15:26 +0300

    Created users app

---
+++ backend/users/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import User
+
+
+@admin.register(User)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = [
+        "username",
+        "email",
+    ]
